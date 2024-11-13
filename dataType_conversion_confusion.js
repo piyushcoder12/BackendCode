e4ee95befe1cb9e4c20c99d32cb361ae61727f9d
piyushcoder12 <coderpiyush369@gmail.com>
2024-11-13 09:29:42 +0530

    datatype

---
+++ dataType_conversion_confusion.js
@@ -0,0 +1,27 @@
+let age= "33"
+
+let score  = " 23r2";
+
+
+console.log(typeof score );
+
+console.log(typeof age);
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber)
+
+
+// let isloggin =  "piyuhsh ";
+
+
+// let booleanisLogged  = boolean (booleanisLogged);
+
+
+let some = 33
+let stringNumber = String(some)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+let booleanisLogged  = boolean (booleanisLogged);
